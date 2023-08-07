af09bf5672befe47d120c1c471e572613994127d
saytaM12 <matyas.oujezdsky@gmail.com>
2023-08-07 22:26:57 +0200

    now???

--- inject/selectAction.js
+++ inject/selectAction.js
@@ -1,8 +1,22 @@
-let text = prompt("\
+function main() { 
+    let def = 'o';
+
+    let url = window.location.href;
+    if (url.includes("https://img3.gelbooru.com/images/") ||
+        url.includes("https://wimg.rule34.xxx//images/") ||
+        url.includes("https://lolibooru.moe/image/") ||
+        url.includes("https://files.yande.re/")) {
+        def = 'd';
+    }
+
+    let text = prompt("\
 s - (first) add to search bar\n\
     u - '-censored'\n\
     s - 'sort:score'\n\
 d - download open images\n\
 s - (not first) switch to previous tab\n\
-o - open correct things in correct tabs\n", "o");
-browser.runtime.sendMessage(text);
+o - open correct things in correct tabs\n", def);
+    browser.runtime.sendMessage(text);
+}
+
+main();
